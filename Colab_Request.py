47a176738bad1a02fbf4d1d12bb9b00f51554e2a
Samson Oladimeji Odunaiya <102425294+Samklugh@users.noreply.github.com>
2022-06-05 15:37:19 +0100

    Add files via upload

---
+++ Colab_Request.py
@@ -0,0 +1,13 @@
+Name= input ('name:' )
+Occupation= input ('occupation: ')
+Project= input ('project: ')
+Country= input ('country: ')
+Phone= input ('phone: ')
+Email= input ('email: ')
+Linkedin= input ('linkedin: ')
+Github= input ('github: ')
+Initials= input ('initials: ')
+
+Colab_Request= f'Hi Samson, My name is {Name}, I am from {Country}, I am a {Occupation}. I love data engineering and data related fields. I am curently working on a project on {Project}. I will love to collaborate with you on a project. Please, feel free to contact me on phone {Phone} or via email {Email}. A detailed overview of my experiences and training are available on my linkedin {Linkedin} and my Github {Github}. I look forward to your reply soon. Best regards {Initials}'
+
+print (Colab_Request)
